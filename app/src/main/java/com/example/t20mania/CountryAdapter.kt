90ebf17b1ab3f2976c37c9b3a509a9a4c5c37378
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-10-22 11:56:09 +0530

    Country RecyclerView Added

---
+++ app/src/main/java/com/example/t20mania/CountryAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.t20mania
+
+import android.content.Context
+import android.graphics.Typeface
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.annotation.RequiresApi
+import androidx.recyclerview.widget.RecyclerView
+
+class CountryAdapter(context: Context):RecyclerView.Adapter<CountryAdapter.CountryViewHolder>() {
+private val ctx:Context= context
+private val countryNames:List<String>
+
+init {
+    val names= context.resources.getStringArray(R.array.countries).toList()
+    countryNames=names
+
+}
+
+class CountryViewHolder(val view:View):RecyclerView.ViewHolder(view)
+{
+    val button= view.findViewById<Button>(R.id.country_button)
+
+}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CountryViewHolder {
+    val layout= LayoutInflater.from(parent.context).inflate(R.layout.item_view,parent,false)
+
+        return CountryViewHolder(layout)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onBindViewHolder(holder: CountryViewHolder, position: Int) {
+        val item= countryNames[position]
+//        val typeface = ctx.resources.getFont(R.font.montserrat_bold)
+//        holder.button.typeface=typeface
+        holder.button.text=item
+
+    }
+
+    override fun getItemCount(): Int {
+return countryNames.size
+    }
+}
--- app/src/main/java/com/example/t20mania/MainActivity.kt
+++ app/src/main/java/com/example/t20mania/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.t20mania
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.t20mania.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView= binding.recyclerView
+        recyclerView.layoutManager= LinearLayoutManager(this)
+        recyclerView.adapter=CountryAdapter(this)
+
     }
 }
