b6e0c7b8d14a8bddf91428a9117faa0eaac9abb0
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-10-23 12:19:34 +0530

    PlayerAdapter string formatting for squad

--- app/src/main/java/com/example/t20mania/PlayersAdapter.kt
+++ app/src/main/java/com/example/t20mania/PlayersAdapter.kt
@@ -1,4 +1,16 @@
 package com.example.t20mania
 
-class PlayersAdapter {
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+
+class PlayersAdapter(private val countryName:String,context:Context): RecyclerView.Adapter<>() {
+
+private val countrySquad:List<String>
+
+init {
+    val sw= countryName.slice(0..2)
+    val p= context.resources.getIdentifier(sw,"array",context.packageName)
+    countrySquad= listOf(p)
+
+}
 }
