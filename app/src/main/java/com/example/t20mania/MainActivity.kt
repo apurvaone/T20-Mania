95d03f43584ff0e9b0e8539b218774ac844b7458
apurvaone <57245377+apurvaone@users.noreply.github.com>
2021-10-25 18:44:10 +0530

    Grid Layout menu enabled

--- app/src/main/java/com/example/t20mania/MainActivity.kt
+++ app/src/main/java/com/example/t20mania/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.t20mania
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.t20mania.databinding.ActivityMainBinding
@@ -9,6 +13,7 @@ import com.example.t20mania.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
+    private  var isLinearLayoutManager= true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -17,7 +22,55 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView= binding.recyclerView
         recyclerView.layoutManager= LinearLayoutManager(this)
-        recyclerView.adapter=CountryAdapter(this)
+//        recyclerView.adapter=CountryAdapter(this)
+        chooseLayout()
 
     }
+
+    private fun chooseLayout()
+    {
+         if(isLinearLayoutManager)
+           recyclerView.layoutManager=LinearLayoutManager(this)
+         else
+             recyclerView.layoutManager=GridLayoutManager(this,2)
+
+         recyclerView.adapter= CountryAdapter(this)
+
+    }
+
+    private  fun setIcon(menuItem: MenuItem?)
+    {
+        if(menuItem==null)
+            return
+
+        menuItem.icon =
+            if (isLinearLayoutManager)
+                ContextCompat.getDrawable(this, R.drawable.ic_grid_layout)
+            else ContextCompat.getDrawable(this, R.drawable.ic_linear_layout)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu,menu)
+
+        val layoutButton= menu?.findItem(R.id.switch_layout)
+        setIcon(layoutButton)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId)
+        {
+            R.id.switch_layout->
+            {
+                isLinearLayoutManager= !isLinearLayoutManager
+                chooseLayout()
+                 setIcon(item)
+                return true
+
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
+
